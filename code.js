6f2b15c4f3f0ada28adc1ad7cdd5c8c5b4d0992d
Doug Sewell <douglasasewell@gmail.com>
2020-04-14 22:07:29 -0400

    Initial styling and some logic added

--- code.js
+++ code.js
@@ -1,24 +1,66 @@
-let score1 = 0;
-let score2 = 0;
+
+//Targets the page's modal on page load
+const introButton = document.getElementById('game-start');
+
+//Initiates the game.
+introButton.addEventListener('click', (e) => {
+    startGame();
+});
 
 
-let milliseconds = Math.ceil(Math.random() * 10000);
 
-setTimeout(() => {
-    sides = Array.from(sides);
-    // sides[0].style.backgroundColor='red';
-    // sides[1].style.backgroundColor='blue';
+const startGame = () => {
+    //Hides intro screen
+    hideIntroModal();
 
-    sides.forEach((side, index) => {
-        side.addEventListener('click', (e) => {
-            if (index == 0) {
-                score1++;
-            } else {
-                score2++;
-            }
-        })
-    });
-    console.log(`Score 1: ${score1} and Score 2: ${score2}`);
-}, milliseconds);
 
-let sides = document.querySelectorAll('.side');
+    let score1 = 0;
+    let score2 = 0;
+
+    //Rounds up between 1 and 10 seconds
+    let milliseconds = Math.ceil(Math.random() * 10000);
+
+
+    //Executes based on milliseconds
+    setTimeout(() => {
+        sides = Array.from(sides);
+        sides[0].style.backgroundColor = 'blue';
+        sides[1].style.backgroundColor = 'red';
+
+        sides.forEach((side, index) => {
+            side.addEventListener('click', (e) => {
+                if (index == 0) {
+                    score1++;
+                    console.log(`Player One wins! Score is ${score1}`);
+                    reset();
+                } else {
+                    score2++;
+                    console.log(`Player Two wins! Score is ${score2}`);
+                    reset();
+                }
+            })
+        });
+        console.log(`Score 1: ${score1} and Score 2: ${score2}`);
+    }, 500); //CHANGE THIS TO MILLISECONDS
+
+    let sides = document.querySelectorAll('.side');
+}
+
+const hideIntroModal = () => {
+    document.getElementById('modal-intro').style.display = 'none';
+}
+
+const reset = () => {
+    console.log('reset activated');
+    document.getElementById('results-board').style.display = 'flex';
+    document.getElementById('modal-intro').style.display = 'flex';
+
+}
+
+/*
+TODO:
+THe results modal is within modal-intro. We need to deparate that because you cannot
+use display:none on modal-intro without it hiding the results-board as well.
+Line 56 was used to highlight this point for now. Commenting it out breaks the app.
+Make the modal and backdrop two different components in the HTML.
+*/
